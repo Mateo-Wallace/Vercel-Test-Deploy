335dd3f88a5019d20f9ec8f45f2888d346f56663
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-09-30 20:45:30 -0400

    test

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 //   app.use(express.static(path.join(__dirname, '../client/.next')));
 // }
 
-// app.use(routes);
+app.use(routes);
 app.use("/home", (req, res) => {
   res.send("Server is running")
 })
