9bc0d0fc57e9b2363c3b75dc07836237706dc115
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-09-30 20:42:43 -0400

    test

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // app.use(cors())
 
@@ -18,7 +18,7 @@ app.use(express.json());
 //   app.use(express.static(path.join(__dirname, '../client/.next')));
 // }
 
-app.use(routes);
+// app.use(routes);
 app.use("/home", (req, res) => {
   res.send("Server is running")
 })
