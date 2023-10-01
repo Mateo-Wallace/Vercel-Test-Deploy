e073cbe068e8aeb622b42f03776bd9e90a04d8f3
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-09-30 20:47:27 -0400

    test

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(cors())
+app.use(cors())
 
 // if we're in production, serve client/build as static assets
 // if (process.env.NODE_ENV === 'production') {
